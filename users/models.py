2f11a98558359e5dd164abba28366d870316ed6d
dazhi <ch2ohch2oh@gmail.com>
2021-01-07 15:01:54 -0500

    Add page to edit user profile

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics')
@@ -10,6 +11,18 @@ class UserProfile(models.Model):
     def __str__(self):
         return f'{self.user.username} profile'
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        from PIL import Image
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+            
+        
 
 @receiver(post_save, sender=User)
 def create_userprofile(sender, instance, created, **kwargs):
