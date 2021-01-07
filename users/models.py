c0719951fabb85cc30308e80f51055227ca2c009
dazhi <ch2ohch2oh@gmail.com>
2021-01-06 23:36:52 -0500

    Add profile page

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics')
+    
+    def __str__(self):
+        return f'{self.user.username} profile'
+
+
+@receiver(post_save, sender=User)
+def create_userprofile(sender, instance, created, **kwargs):
+    print('create_profile signal triggered')
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_userprofile(sender, instance, **kwargs):
+    instance.userprofile.save()
