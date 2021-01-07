c145d4fa3cb681aea7c554516711c903ac8cb94a
dazhi <ch2ohch2oh@gmail.com>
2021-01-07 16:22:40 -0500

    Finish CRUD of posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
+
 from . import views
+from .views import PostDetailView, PostListView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', views.PostListView.as_view(), name='blog-home'),
+    # The `PostDetailView` expects a variable called `pk` by default
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    # The `PostCreateView` expects a template called `post_form.html` by default
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    # The `PostDeleteView` will ask for confirmation
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,77 @@
+from django.contrib.messages.api import success
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 from django.http import HttpResponse
 
 from .models import Post
 
-# Create your views here.
+class PostListView(ListView):
+    
+    model = Post
+    
+    # The default template is 'blog/post_list.html'
+    template_name = 'blog/home.html'
+    
+    # Otherwise the object name is `object`
+    context_object_name = 'posts'
+    
+    # Order from newest to oldest
+    ordering = ['-date_posted']
+    
+    class Meta:
+        pass
+
+class PostDetailView(DetailView):
+    
+    model = Post
+    
+    context_object_name = 'post'
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    
+    model = Post
+    
+    success_url = '/'
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+# Put the mixin as the first!!!
+class PostCreateView(LoginRequiredMixin, CreateView):
+    
+    model = Post
+    
+    # Need to tell what do you what in the form
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        '''Set the author of the post to be the current user'''
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+# Make sure a user can only update his own posts
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    
+    model = Post
+    
+    # Need to tell what do you what in the form
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        '''Set the author of the post to be the current user'''
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        # The post we are trying to update
+        post = self.get_object()
+        return self.request.user == post.author
+        
 def home(request):
     context = {
         'posts': Post.objects.all(),
