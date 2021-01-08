9acff1821669893c73578a1243d8f8fc9a5be116
dazhi <ch2ohch2oh@gmail.com>
2021-01-08 15:38:51 -0500

    Make email unique and rename profile pics

--- users/models.py
+++ users/models.py
@@ -1,12 +1,22 @@
+from django.core.files import uploadhandler
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 
+import os
+
+User._meta.get_field('email')._unique = True
+
+def rename_profile_pic(instance, filename):
+    upload_to = 'profile_pics'
+    ext = filename.split('.')[-1]
+    new_filename = f'{instance.pk}.{ext}'
+    return os.path.join(upload_to, new_filename)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='profile_pics/default.jpg', upload_to=rename_profile_pic)
     
     def __str__(self):
         return f'{self.user.username} profile'
